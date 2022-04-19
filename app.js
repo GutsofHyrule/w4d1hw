54d8b75b31fbd65603b94c26d1bbb3fb2a5d648f
Spencer <dresmanns@gmail.com>
2022-04-18 21:45:39 -0400

    l

---
+++ app.js
@@ -0,0 +1,32 @@
+
+    const myArray = [];
+for(let i = 1; i < 11; i++){
+    myArray.push(i);
+}
+console.log(myArray);
+
+
+
+
+
+const anotherArray = [];
+var i = 1;
+
+while(i < 11){
+  anotherArray.push(i);
+  i++;
+}
+console.log(anotherArray);
+
+
+var threeArray = [];
+for(let i = 11; i < 23; i += 2){
+    threeArray.push(i);
+}
+console.log(threeArray);
+
+var fourArray = [];
+for(let i = 15; i > 0; i -= 2){
+    fourArray.push(i);
+}
+console.log(fourArray);
